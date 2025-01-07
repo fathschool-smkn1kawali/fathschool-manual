b9fd554c992496c00d17fc512f30fe3cf66fce35
webfathforce <134572236+webfathforce@users.noreply.github.com>
2025-01-07 16:53:40 +0700

    feat: Feature Theme Dark Mode

--- src/components/fragments/ButtonsFloat.tsx
+++ src/components/fragments/ButtonsFloat.tsx
@@ -1,16 +1,18 @@
+import { useTheme } from "@/lib/utils/useTheme";
 import { Icons } from "@/resource";
 import { Button, Tooltip } from "@nextui-org/react";
 import Link from "next/link";
 
 export const ButtonsFloat = () => {
+  const { isDarkMode: isTheme, toggleDarkMode: toggleTheme } = useTheme();
   const linkCustomer = 'https://api.whatsapp.com/send?phone=6285161721727&text=Halo%20selamat%20datang%20di%20WhatsApp%20admin%20FathForce'
   
   return (
     <div className="fixed bottom-2 right-2 sm:bottom-8 sm:right-8 z-50">
       <div className="flex flex-col gap-2 sm:gap-4">
         <Tooltip content="Theme" placement="left">
-          <Button isIconOnly>
-            <Icons.Moon />
+          <Button isIconOnly onClick={toggleTheme}>
+            {isTheme ? <Icons.Sun /> : <Icons.Moon />}
           </Button>
         </Tooltip>
         <Tooltip content="Hubungi Customer" placement="left">
---
+++ src/lib/utils/useTheme.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export function useTheme (): { isDarkMode: boolean; toggleDarkMode: () => void } {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (storedTheme === "dark" || (!storedTheme && prefersDark)) {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
+
+  return { isDarkMode, toggleDarkMode};
+};
